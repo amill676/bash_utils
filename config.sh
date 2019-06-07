f2bc57212e74d7a0934b7704db014e2366b05975
Adam Miller <amill676@gmail.com>
2019-06-07 12:24:23 -0700

    change command prompt

--- config.sh
+++ config.sh
@@ -1,4 +1,8 @@
-export PS1="\w$ "
+# The following will only display the working directory. However if you
+# set this on a machine you ssh into, it is hard to tell the difference
+# between your local and remote terminals on the local machine. So I am
+# disabling for now until I find a better way
+#export PS1="\w$ "
 
 # virtual env
 export WORKON_HOME=~/virtualenvs
